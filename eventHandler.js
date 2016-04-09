ab70c736cb0ecf7e1c0c9e985f4b276938f22785
Bradley Guthrie <guppy96@gmail.com>
2016-04-09 12:14:58 -0500

    removed whitespace

--- eventHandler.js
+++ eventHandler.js
@@ -1,12 +1,11 @@
-// The second IIFE should augment the original one with a function that creates 
-// all of the eventHandlers that you need for the application. Name the function 
+// The second IIFE should augment the original one with a function that creates
+// all of the eventHandlers that you need for the application. Name the function
 //activateEvents.
-
 //build event handler function
 var CarLot = (function(eventHandlers) {
   eventHandlers.activateEvents = function() {
-    //build array for the the card 
-    var carCard = document.getElementsByClassName("card"); 
+    //build array for the the card
+    var carCard = document.getElementsByClassName("card");
     //loop through the array listening for what element was clicked
     //when card is slected add the border and color and call the function
     //to change the text on the card  Requirement 8 & 9
@@ -22,9 +21,9 @@ var CarLot = (function(eventHandlers) {
     //add event listener on text box area on key up and call function
     var enteredText = document.getElementById("textBox");
     enteredText.addEventListener("keyup", function(event, changedCarCard){
-      //modify text on card requirement 9        
+      //modify text on card requirement 9
       CarLot.editText();
-    });  
+    });
   };
   return eventHandlers
-}(CarLot || {}));  
+}(CarLot || {}));
