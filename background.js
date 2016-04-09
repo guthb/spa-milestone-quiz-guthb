5b96ac6e0a73935362ca8940f4769c6f16398f80
Bradley Guthrie <guppy96@gmail.com>
2016-04-09 12:10:02 -0500

    removed whitespace

--- background.js
+++ background.js
@@ -1,18 +1,17 @@
-//The final IIFE should augment the object with two more functions. 
-//One function resets the border thickness and background color for each car 
-//element back to the original values. The other function changes the thickness 
-//of the border of a car element, and changes its background color. The 
-//function must accept two arguments. A car DOM element that was clicked on 
+//The final IIFE should augment the object with two more functions.
+//One function resets the border thickness and background color for each car
+//element back to the original values. The other function changes the thickness
+//of the border of a car element, and changes its background color. The
+//function must accept two arguments. A car DOM element that was clicked on
 //& A color name
-
-var CarLot = (function(background) {   
+var CarLot = (function(background) {
   background.resetBorderThickness = function() {
-  //function resets the border thickness and background color for each car  
+  //function resets the border thickness and background color for each car
     var cards = document.getElementsByClassName("card");
     for (var i = 0; i < cards.length; i++) {
        cards[i].style.borderWidth = "thin";
        cards[i].classList.remove("userSelected");
-     }; 
+     };
   };
   //function changes the thickness of the border of a car element, and changes its background color.
   background.addBorderThickness = function(selected) {
@@ -23,9 +22,9 @@ var CarLot = (function(background) {
   //function  that replaces the value from the text box
   background.editText = function() {
     var carListing = document.getElementById("textBox").value;
-    var selectedCard = document.getElementsByClassName("userSelected")   
+    var selectedCard = document.getElementsByClassName("userSelected")
     //Check for nulls on the selected card
-    if (selectedCard.length > 0) {     
+    if (selectedCard.length > 0) {
       var descritption = selectedCard[0].getElementsByTagName("p");
       descritption[0].innerHTML = carListing;
     }
